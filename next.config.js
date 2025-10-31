777abab21dae9559bf66cf8d5d299ff128e1406a
Pavel Galante <pavelgalante@gmail.com>
2025-10-31 10:29:01 +0300

    Fix CSP headers to allow CloudPayments widget and APIs

--- next.config.js
+++ next.config.js
@@ -6,6 +6,28 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: [
+              "default-src 'self'",
+              "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://widget.cloudpayments.ru https://api.cloudpayments.ru https://intent-api.cloudpayments.ru",
+              "style-src 'self' 'unsafe-inline'",
+              "img-src 'self' data: https: https://widget.cloudpayments.ru https://ads.cloudpayments.ru",
+              "font-src 'self' data:",
+              "connect-src 'self' https://widget.cloudpayments.ru https://api.cloudpayments.ru https://intent-api.cloudpayments.ru wss://widget.cloudpayments.ru",
+              "frame-src 'self' https://widget.cloudpayments.ru https://stiger.app",
+              "frame-ancestors 'self' https://widget.cloudpayments.ru"
+            ].join('; ')
+          }
+        ]
+      }
+    ]
+  }
 }
 
 module.exports = nextConfig
